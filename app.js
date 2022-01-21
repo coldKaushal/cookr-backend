af8530b5f5bf0da0331164603632b3a80a6349e9
coldkaushal <kauagg111@gmail.com>
2022-01-21 23:58:37 +0530

    add authentication

---
+++ app.js
@@ -0,0 +1,111 @@
+require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const session = require('express-session');
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended:true
+}));
+
+
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+  }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb://localhost:27017/Cookr");
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = new mongoose.model("Credential", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+});
+
+
+app.get("/", function(req, res){
+    if(req.isAuthenticated()){
+        console.log("Already logged in");
+        res.send("Already logged in");
+    }else{
+        console.log("Not logged in");
+        res.send("not logged in");
+    }
+});
+
+app.get("/logout", function(req, res){
+    req.logout();
+    res.send("successfully logged out");
+})
+
+
+
+app.post("/login", function(req, res){
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
+    console.log(user);
+    req.login(user, function(err){
+        if(err){
+            console.log(err);
+        }else{
+            let a=0;
+            passport.authenticate("local")(req, res, function(){
+                a=1;
+                res.send("successfully logged in");
+            })
+            console.log(a);
+        }
+    })
+});
+
+app.post("/signup", function(req, res){
+    console.log(req.body);
+    User.register({username: req.body.username}, req.body.password, function(err, user){
+        if(err){
+            console.log(err);
+            res.send(err);
+        }else{
+            let a=0;
+            passport.authenticate("local")(req, res, function(){
+                a=1;
+                console.log("Successfully logged in");
+                res.send("Successfully registered");
+            })
+            console.log(a);
+        }
+    })
+});
+
+
+
+app.listen(4000, function(){
+    console.log("Server started at port 4000");
+})
